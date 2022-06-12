1e4b70057ecc5ce86301d49c3cdc6b819791290f
yjkim0109 <yjkkim0109@gmail.com>
2022-06-12 22:48:58 +0900

    pracice

--- coordinates/main.js
+++ coordinates/main.js
@@ -25,16 +25,33 @@ const vertical = document.querySelector(".vertical");
 const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
 
-function move(event) {
-  const x = event.clientX;
-  const y = event.clientY;
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  coordinates.style.left = `${x}px`;
-  coordinates.style.top = `${y}px`;
-  coordinates.innerText = `${x}px, ${y}px`;
-}
 
-document.addEventListener("mousemove", move);
+// function move(event) {
+//   const x = event.clientX;
+//   const y = event.clientY;
+//   vertical.style.left = `${x}px`;
+//   horizontal.style.top = `${y}px`;
+//   target.style.left = `${x}px`;
+//   target.style.top = `${y}px`;
+//   coordinates.style.left = `${x}px`;
+//   coordinates.style.top = `${y}px`;
+//   coordinates.innerText = `${x}px, ${y}px`;
+// }
+
+
+
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener('mousemove', event => {
+    const x = event.clientX;
+    const y = event.clientY;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+    coordinates.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
+    coordinates.innerText = `${x}px, ${y}px`;
+  });
+})
