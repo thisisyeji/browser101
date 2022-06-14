5ddff1a202a688d3649a203599d3817d3fb6cd6e
yjkim0109 <yjkkim0109@gmail.com>
2022-06-14 11:36:34 +0900

    second

---
+++ shopping list/README.md
@@ -0,0 +1,50 @@
+# Shopping list
+
+This is a toy project using HTML, CSS and JavaScript.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Function](#function)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### Function
+
+Users should be able to:
+
+- Add items
+- Delete items
+- Check items
+
+### Screenshot
+
+![](screenshot.png)
+
+### Links
+
+- Live Site URL: []()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS
+- JavaScript
+
+### What I learned
+
+```css
+
+```
+
+## Author
+
+- Website - [Yeji Kim](https://github.com/yjkim0109)
--- shopping list/main.js
+++ shopping list/main.js
@@ -1,15 +1,42 @@
 const listForm = document.querySelector("#list_form");
 const list = document.querySelector("#list");
 const inputText = document.querySelector("#input_text");
-const inputValue = inputText.value;
+const title = document.querySelector(".title");
+
+const date = new Date();
+const today = `
+${date.getFullYear()}. ${date.getMonth()}. ${date.getDate()} 
+`; 
+title.append(today);
+
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function lineDone(event) {
+  const span = event.target
+  span.classList.toggle("line_through")
+}
 
 listForm.addEventListener("submit", () => {
-  list.innerHTML = `
-  <li class="list_item">
-  <input type="checkbox" class="checkbox">
-  ${inputValue}
-  <i class="fa-regular fa-square-minus"></i>
-  </li>
-  `
+  const li = document.createElement("li")
+  const span = document.createElement("span")
+  const button = document.createElement("button");
+  button.textContent = "X";
+
+  list.append(li)
+  li.setAttribute('class', 'list_item')
+  li.appendChild(span)
+  span.textContent = inputText.value;
+  li.appendChild(button);
+  button.setAttribute('class', 'delete_btn')
+  button.addEventListener("click", deleteToDo);
+  span.addEventListener("click", lineDone);
+
+  inputText.value = "";
 })
+
+
 
